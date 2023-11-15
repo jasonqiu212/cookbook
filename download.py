d2a38e8fa0613287c340f3da9261fdab050c4407
jasonqiu212 <jason_qiu@hotmail.com>
2023-11-14 21:08:48 -0700

    Add methods to load mock recipes and load recipes

--- download.py
+++ download.py
@@ -3,23 +3,56 @@
 import json
 
 
-def download():
+def download_recipe_by_name(query):
     """
-    Loads recipe from API.
+    Downloads recipe from API through searching meal by name.
+
+    Args:
+        query: Search query for meal
 
     Returns:
-        Dictionary representing ingredients and steps of recipe.
+        Dictionary containing recipe name, instructions, and ingredients of searched recipe
     """
     # TODO: Change as per required for chosen API
+    data = {}
+    return load_recipe(data)
 
 
-def load_test_recipe():
+def load_mock_recipe(file_name):
     """
     Loads test recipe JSON file.
 
+    Args:
+        file_name: File name containing mock recipe
+
     Returns:
-        Dictionary representing ingredients and steps of recipe.
+        Dictionary containing recipe name, instructions, and ingredients of mock recipe
     """
-    # TODO: Change as per required for chosen API
-    with open("recipe.json") as json_file:
-        return json.load(json_file)
+    with open(file_name) as json_file:
+        data = json.load(json_file)
+        return load_recipe(data)
+
+
+def load_recipe(data):
+    """
+    Loads recipe returned by API into relevant data.
+
+    Args:
+        data: Recipe returned by API
+
+    Returns:
+        Dictionary containing recipe name, instructions, and ingredients
+    """
+    recipe = {}
+    recipe['name'] = data['meals'][0]['strMeal']
+    recipe['instructions'] = data['meals'][0]['strInstructions']
+    recipe['ingredients'] = {}
+    for i in range(1, 21):
+        ingredient = data['meals'][0]['strIngredient{}'.format(i)]
+        measure = data['meals'][0]['strMeasure{}'.format(i)]
+
+        if ingredient == '' or measure == '':
+            break
+
+        recipe['ingredients'][ingredient] = measure
+    return recipe
