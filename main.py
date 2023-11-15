fa22f0482c9490a12f6c5a34c687a97578f3c74f
jasonqiu212 <jason_qiu@hotmail.com>
2023-11-14 21:09:51 -0700

    Include calls to download methods with customizable constants

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from download import download
+"""This file serves as the entry point of the program."""
+
+from download import download_recipe_by_name, load_mock_recipe
 from extract import extract
 
 
 def main():
-    recipe = download()
+    # Change following constants if needed
+    RECIPE_NAME_QUERY = ''
+    MOCK_RECIPE_FILE_NAME = 'mock-recipe-1.json'
+
+    # recipe = download_recipe_by_name(RECIPE_NAME_QUERY)
+    recipe = load_mock_recipe(MOCK_RECIPE_FILE_NAME)
     results = extract(recipe)
     print(results)
 
