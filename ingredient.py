1e24c2d4932dbd67e930fbf067b6c0ee3f3f88cf
jasonqiu212 <jason_qiu@hotmail.com>
2023-11-15 22:32:11 -0600

    Add class for ingredient

---
+++ ingredient.py
@@ -0,0 +1,9 @@
+class Ingredient:
+    """
+    Class representing an ingredient.
+    """
+
+    def __init__(self, name, quantity, measurement):
+        self.name = name
+        self.steps = quantity
+        self.ingredients = measurement
