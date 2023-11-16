54104c9641db52ddc4b49dc239df69bcafb239d9
jasonqiu212 <jason_qiu@hotmail.com>
2023-11-15 21:59:15 -0600

    Add class to represent parsed recipe

---
+++ parsed_recipe.py
@@ -0,0 +1,9 @@
+class ParsedRecipe:
+    """
+    Class representing a parsed recipe.
+    """
+
+    def __init__(self, name, steps, ingredients):
+        self.name = name
+        self.steps = steps
+        self.ingredients = ingredients
