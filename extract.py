00584ffc6015f3861d08137dc4e439bb46f967a4
jasonqiu212 <jason_qiu@hotmail.com>
2023-11-22 22:42:42 -0600

    Lint

--- extract.py
+++ extract.py
@@ -8,20 +8,25 @@ import spacy
 # TODO: Method for extracting tools, utensils, and parameters
 nlp = spacy.load("en_core_web_sm")
 
+
 def is_imperative(sentence):
     """
     Checks if sentence is imperative.
+
+    Args:
+        sentence: Sentence to check
+
+    Returns:
+        True, if sentence is imperative. False otherwise.
     """
-    # TODO
-    # Parse the sentence using the spaCy model
     doc = nlp(sentence)
 
     for token in doc:
-        if token.dep_ == "ROOT" and token.pos_ == "VERB":
-            # Check if there's no subject
-            if not any(child.dep_ in ["nsubj", "nsubjpass"] for child in token.children):
+        if token.dep_ == "ROOT" and token.pos_ == "VERB" and token.tag_ == "VB":
+            has_no_explicit_subject = not any(
+                child.dep_ in ["nsubj", "nsubjpass"] for child in token.children)
+            if has_no_explicit_subject:
                 return True
-
     return False
 
 
@@ -42,6 +47,3 @@ def extract(raw_recipe):
     # TODO: Include extraction methods here
 
     return name, steps, ingredients
-
-
-print(is_imperative('here is an apple'))
