cf6558c466d34b3ce643a057c6e94a13f09a3b6d
jasonqiu212 <jason_qiu@hotmail.com>
2023-11-24 20:52:05 -0600

    Add method to get step i from recipe

--- parsed_recipe.py
+++ parsed_recipe.py
@@ -7,3 +7,24 @@ class ParsedRecipe:
         self.name = name
         self.steps = steps
         self.ingredients = ingredients
+
+    def get_step(self, i):
+        """
+        Gets the i-th step of the recipe.
+
+        Args:
+            i: Index of step to retrieve.
+
+        Returns:
+            i-th step of the recipe. IndexError, if index is out of bounds.
+
+        Raises:
+            ValueError: If i is None.
+            IndexError: If i is out of bounds.
+
+        """
+        if i == None:
+            raise ValueError
+        if i < 0 or i >= len(self.steps):
+            raise IndexError
+        return self.steps[i]
