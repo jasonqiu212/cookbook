f36a42dc38a42f98d87a23a2d4db29fe068f3b27
jasonqiu212 <jason_qiu@hotmail.com>
2023-12-01 11:19:49 -0600

    Add method to check ingredient similartiy for ingredient extraction

--- ingredient.py
+++ ingredient.py
@@ -1,3 +1,6 @@
+import editdistance
+
+
 class Ingredient:
     """
     Class representing an ingredient.
@@ -9,6 +12,18 @@ class Ingredient:
         self.name = name
         self.quantity = quantity
         self.measurement = measurement
+
+    def is_similar(self, str):
+        """
+        Checks if this ingredient's name is similar to given string using edit distance.
+
+        Args:
+            str: String to check similarity with.
+
+        Returns:
+            True, if similar. False otherwise.
+        """
+        return editdistance.eval(str, self.name) < 5
 
     def __repr__(self):
         if self.measurement == Ingredient.COUNTABLE_MEASUREMENT:
