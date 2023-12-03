b00d4cc7fef46f93dc850e65505d04da36f1b6d7
jasonqiu212 <jason_qiu@hotmail.com>
2023-12-03 11:47:47 -0600

    Add conversion of units in steps

--- step.py
+++ step.py
@@ -1,6 +1,6 @@
 import re
 
-from units import convert_celsius_to_fahrenheit, convert_fahrenheit_to_celsius
+from units import convert_celsius_to_fahrenheit, convert_fahrenheit_to_celsius, convert_imperial_to_metric, convert_metric_to_imperial, IMPERIAL_TO_METRIC, METRIC_TO_IMPERIAL
 
 
 class Step:
@@ -47,14 +47,14 @@ class Step:
         temperature_parameters = self.get_temperature_parameters()
 
         to_match = '\d*\.?\d+°C' if target_unit == 'IMPERIAL' else '\d*\.?\d+°F'
-        f = convert_celsius_to_fahrenheit if target_unit == 'IMPERIAL' else convert_fahrenheit_to_celsius
+        f_temp = convert_celsius_to_fahrenheit if target_unit == 'IMPERIAL' else convert_fahrenheit_to_celsius
 
         converted_parameters = []
 
         for p in temperature_parameters:
             while re.search(to_match, p):
                 temperature = float(re.search(to_match, p).group()[:-2])
-                converted, unit = f(temperature)
+                converted, unit = f_temp(temperature)
                 p = re.sub(to_match, str(converted) + unit, p, 1)
             converted_parameters.append(p)
 
@@ -62,6 +62,19 @@ class Step:
 
         for original, converted in zip(temperature_parameters, converted_parameters):
             self.text = self.text.replace(original, converted)
+
+        units = METRIC_TO_IMPERIAL if target_unit == 'IMPERIAL' else IMPERIAL_TO_METRIC
+        f = convert_metric_to_imperial if target_unit == 'IMPERIAL' else convert_imperial_to_metric
+
+        for unit in reversed(units.keys()):
+            if re.search(f'\d*\.?\d+ {unit}', self.text):
+                m = re.search(f'\d*\.?\d+ {unit}', self.text).group()
+                print(m)
+                q_str = m.replace(f' {unit}', '')
+                q = int(q_str) if q_str.isnumeric() else float(q_str)
+                converted_quantity, converted_measurement = f(q, unit)
+                self.text = self.text.replace(m, ' '.join(
+                    [str(converted_quantity), converted_measurement]))
 
     def __repr__(self):
         return self.text
